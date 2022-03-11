6303937402a13b60c6740bf2e0f5bd993eec1275
Jnqa <ligez@yandex.ru>
2022-03-11 12:45:37 +0300

    Python update

---
+++ send.py
@@ -0,0 +1,13 @@
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+client = WebClient(token="${{ inputs.SLACK_TOKEN }}")
+
+if "${{ inputs.FILENAME }}":
+  result = client.files_upload(channels="${{ inputs.CHANNELS }}",
+                               initial_comment="""${{ inputs.MESSAGE_TEXT }}""",
+                               file="${{ inputs.FILENAME }}",)
+  print(f"Result is:\n{result}")
+else:
+  response = client.chat_postMessage(channel="${{ inputs.CHANNELS }}", text="""${{ inputs.MESSAGE_TEXT }}""" )
+  print(response)
