eaac9e0a2c3830a8ee532706ba85d93f6e4002c7
Jnqa <ligez@yandex.ru>
2022-03-11 12:48:41 +0300

    update python

--- send.py
+++ send.py
@@ -3,11 +3,14 @@ from slack_sdk.errors import SlackApiError
 
 client = WebClient(token="${{ inputs.SLACK_TOKEN }}")
 
-if "${{ inputs.FILENAME }}":
-  result = client.files_upload(channels="${{ inputs.CHANNELS }}",
-                               initial_comment="""${{ inputs.MESSAGE_TEXT }}""",
-                               file="${{ inputs.FILENAME }}",)
-  print(f"Result is:\n{result}")
-else:
-  response = client.chat_postMessage(channel="${{ inputs.CHANNELS }}", text="""${{ inputs.MESSAGE_TEXT }}""" )
-  print(response)
+try:
+    if "${{ inputs.FILENAME }}":
+        result = client.files_upload(channels="${{ inputs.CHANNELS }}",
+                                   initial_comment="""${{ inputs.MESSAGE_TEXT }}""",
+                                   file="${{ inputs.FILENAME }}",)
+        print(f"Result is:\n{result}")
+    else:
+        response = client.chat_postMessage(channel="${{ inputs.CHANNELS }}", text="""${{ inputs.MESSAGE_TEXT }}""" )
+        print(response)
+except Exception as error:
+    print(f"Error: \n{error}")
