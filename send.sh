2a599b7fa28aa16afab8c9a4ae6aeae514bc4436
Jnqa <ligez@yandex.ru>
2022-07-06 13:47:20 +0300

    clean bash, add readme

--- send.sh
+++
@@ -1,5 +0,0 @@
-curl -F file=@${{ inputs.FILENAME }} \
-        -F "initial_comment=${{ inputs.MESSAGE_TEXT }}" \
-        -F channels=${{ inputs.CHANNELS }} \
-        -H "Authorization: Bearer ${{ inputs.SLACK_TOKEN }}" \
-        https://slack.com/api/files.upload
